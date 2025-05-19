0f46a94b60a160a7c9b18c1dc6d42efb6ffe9fda
sashank-27 <bn.sashank@gmail.com>
2025-05-19 23:01:00 +0530

    port added

--- app.py
+++ app.py
@@ -320,4 +320,5 @@ def get_stats():
     return jsonify(stats_copy)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
